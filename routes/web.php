31f92e1c4e4ebe8259f37661a9d993ecc5769758
srywahyuni12 <wsriyuni34@gmail.com>
2019-10-08 17:56:50 +0800

    Tugas10

--- routes/web.php
+++ routes/web.php
@@ -19,40 +19,11 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/kategori_artikel', 'KategoriArtikelController@index')->name('kategori_artikel.index');
-Route::get('/kategori_artikel/create', 'KategoriArtikelController@create')->name('kategori_artikel.create');
-Route::post('/kategori_artikel','KategoriArtikelController@store')->name('kategori_artikel.store');
-Route::get('/kategori_artikel/{id}','KategoriArtikelController@show')->name('kategori_artikel.show');
-Route::get('/kategori_artikel/{id}/edit','KategoriArtikelController@edit')->name('kategori_artikel.edit');
-Route::patch('/kategori_artikel/{id}','KategoriArtikelController@update')->name('kategori_artikel.update');
-Route::delete('/kategori_artikel/{id}','KategoriArtikelController@destroy')->name('kategori_artikel.destroy');
 
-Route::get('/kategori_berita', 'KategoriBeritaController@index')->name('kategori_berita.index');
-Route::get('/kategori_berita/create', 'KategoriBeritaController@create')->name('kategori_berita.create');
-Route::post('/kategori_berita','KategoriBeritaController@store')->name('kategori_berita.store');
-Route::get('/kategori_berita/{id}','KategoriBeritaController@show')->name('kategori_berita.show');
-Route::get('/kategori_berita/{id}/edit','KategoriBeritaController@edit')->name('kategori_berita.edit');
-Route::patch('/kategori_berita/{id}','KategoriBeritaController@update')->name('kategori_berita.update');
-Route::delete('/kategori_berita/{id}','KategoriBeritaController@destroy')->name('kategori_berita.destroy');
-
-Route::get('/kategori_galeri', 'KategoriGaleriController@index')->name('kategori_galeri.index');
-Route::get('/kategori_galeri/create', 'KategoriGaleriController@create')->name('kategori_galeri.create');
-Route::post('/kategori_galeri','KategoriGaleriController@store')->name('kategori_galeri.store');
-Route::get('/kategori_galeri/{id}','KategoriGaleriController@show')->name('kategori_galeri.show');
-Route::get('/kategori_galeri/{id}/edit','KategoriGaleriController@edit')->name('kategori_galeri.edit');
-Route::patch('/kategori_galeri/{id}','KategoriGaleriController@update')->name('kategori_galeri.update');
-Route::delete('/kategori_galeri/{id}','KategoriGaleriController@destroy')->name('kategori_galeri.destroy');
-
-
-Route::get('/kategori_pengumuman', 'KategoriPengumumanController@index')->name('kategori_pengumuman.index');
-Route::get('/kategori_pengumuman/create', 'KategoriPengumumanController@create')->name('kategori_pengumuman.create');
-Route::post('/kategori_pengumuman','KategoriPengumumanController@store')->name('kategori_pengumuman.store');
-Route::get('/kategori_pengumuman/{id}','KategoriPengumumanController@show')->name('kategori_pengumuman.show');
-Route::get('/kategori_pengumuman/{id}/edit','KategoriPengumumanController@edit')->name('kategori_pengumuman.edit');
-Route::patch('/kategori_pengumuman/{id}','KategoriPengumumanController@update')->name('kategori_pengumuman.update');
-Route::delete('/kategori_pengumuman/{id}','KategoriPengumumanController@destroy')->name('kategori_pengumuman.destroy');
-
-
+Route::resource('kategori_artikel','KategoriArtikelController');
+Route::resource('kategori_berita','KategoriBeritaController');
+Route::resource('kategori_galeri','KategoriGaleriController');
+Route::resource('kategori_pengumuman','KategoriPengumumanController');
 
 
 Route::get('/artikel', 'artikelController@index')->name('artikel.index');
